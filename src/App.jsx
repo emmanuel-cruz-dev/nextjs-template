71aebe2bcd947c91fdbcae241a6c1804353e8b75
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-19 16:48:08 -0300

    agregando navigate a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
+
+const NAVIGATION_EVENT = "pushstate";
+
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  // Crear un evento personalizado
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
+}
 
 function HomePage() {
   return (
@@ -32,6 +42,18 @@ function AboutPage() {
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+    };
+  }, []);
+
   return (
     <main>
       {currentPath == "/" && <HomePage />}
