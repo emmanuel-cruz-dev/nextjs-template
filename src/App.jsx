38ede47fe3f7910887fe2419fcd897b59153bd9e
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-20 13:00:53 -0300

    lazy loading y suspense agregados a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { lazy, Suspense } from "react";
 import "./App.css";
 import HomePage from "./pages/Home";
-import AboutPage from "./pages/About";
-import { Router } from "./Router";
+// import AboutPage from "./pages/About";
+const LazyAboutPage = lazy(() => import("./pages/About"));
 import Page404 from "./pages/404";
 import SearchPage from "./pages/SearchPage";
+
+import { Router } from "./Router";
 import Route from "./Route";
 
 const appRoutes = [
@@ -16,10 +19,12 @@ const appRoutes = [
 function App() {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404}>
-        <Route path="/" Component={HomePage} />
-        <Route path="/about" Component={AboutPage} />
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/about" Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   );
 }
