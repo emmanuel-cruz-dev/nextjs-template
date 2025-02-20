9047a0a4324bf7dd9dd60e9f3795d6552f4e8e9c
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-20 10:27:31 -0300

    separando Router en un componente aparte

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { EVENTS } from "./consts";
 import "./App.css";
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
+import { Router } from "./Router";
 
 const routes = [
   {
@@ -15,29 +13,6 @@ const routes = [
     Component: AboutPage,
   },
 ];
-
-function Router({
-  routes = [],
-  defaultComponent: DefaultComponent = () => null,
-}) {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname);
-    };
-
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange);
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange);
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange);
-      window.removeEventListener(EVENTS.POPSTATE, onLocationChange);
-    };
-  }, []);
-
-  const Page = routes.find(({ path }) => path === currentPath)?.Component;
-  return Page ? <Page /> : <DefaultComponent />;
-}
 
 function App() {
   return (
