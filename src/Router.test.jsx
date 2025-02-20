012e29ef913e96cc91f00bb624981c512fbf4a4a
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-20 14:37:02 -0300

    happy-dom instalado

--- src/Router.test.jsx
+++ src/Router.test.jsx
@@ -1,7 +1,9 @@
 import { describe, it, expect } from "vitest";
+import { render } from "@testing-library/react";
+import { Router } from "./Router";
 
 describe("Router", () => {
-  it("should work", () => {
-    expect(1).toBe(1);
+  it("should render without problems", () => {
+    render(<Router routes={[]} />);
   });
 });
