7a9801d23d5bbf1ba54cca87b237a97b6ed48539
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-11 17:32:23 -0300

    feat: adds lusitana to app/ui/fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,13 @@
-import { Montserrat } from "next/font/google";
+import { Lusitana, Montserrat } from "next/font/google";
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
   display: "swap",
 });
