81a74d0d89986dea41ba76827efd1d9379ec860c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-13 14:06:36 -0300

    feat: adds SideNav to app/dashboard/layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,16 @@
+import SideNav from "../ui/dashboard/sidenav";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section>
-      Esto es el layout del dashboard
-      {children}
-    </section>
+    <div className="flex h-screen flex-col md:flex-row">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
   );
 }
